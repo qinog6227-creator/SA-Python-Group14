c8997a76f688efc8bdad93d7f5f7fb1d4faffa9b
qinog6227-creator <qinog6227@gmail.com>
2025-12-09 12:14:45 +0900

    物理レベルでみんなのを合わせます

--- 物理レベルMarge/Mmain.py
+++ 物理レベルMarge/Mmain.py
@@ -1,9 +1,9 @@
 #メイン関数。すべて呼び出す
 
-import Ltitle
-import Lmap
-import LBbattle_main
-import Lresult
+import Mtitle
+import Mmap
+import Mbattle_main
+import Mresult
 
 #------変数一覧---------
 game_state = 0
@@ -12,50 +12,50 @@ is_move = 'none'
 is_win = 'none'
 encount = 0 #バトル数を計測
 
-Ltitle.draw_title()
+Mtitle.draw_title()
 while is_start != 's':
-    is_start = input(Ltitle.draw_wait_start())
+    is_start = input(Mtitle.draw_wait_start())
     if is_start == 's':
         break
 
-Lmap.draw_map1()
+Mmap.draw_map1()
 while is_move != 'y':
-    is_move = input(Lmap.draw_wait_fowrd())
+    is_move = input(Mmap.draw_wait_fowrd())
     if is_move == 'y':
         break
 
 encount += 1
-is_win = LBbattle_main.run_battle(encount) #LBattle_mainを呼び出す
+is_win = Mbattle_main.run_battle(encount) #LBattle_mainを呼び出す
 if is_win == 'win':
-    Lresult.draw_wining()
+    Mresult.draw_wining()
 elif is_win == 'lose':
-    Lresult.draw_losing()
+    Mresult.draw_losing()
 is_move = 'none' #★　次のステージへ進むか判定を初期化
 is_win = 'none'
-Lmap.draw_map2()
+Mmap.draw_map2()
 while is_move != 'y':
-    is_move = input(Lmap.draw_wait_fowrd())
+    is_move = input(Mmap.draw_wait_fowrd())
     if is_move == 'y':
         break
 
 encount += 1
-is_win = LBbattle_main.run_battle(encount) #LBattle_mainを呼び出す
+is_win = Mbattle_main.run_battle(encount) #LBattle_mainを呼び出す
 if is_win == 'win':
-    Lresult.draw_wining()
+    Mresult.draw_wining()
 elif is_win == 'lose':
-    Lresult.draw_losing()
+    Mresult.draw_losing()
 
 is_move = 'none' #★　次のステージへ進むか判定を初期化
 is_win = 'none'
-Lmap.draw_map3()
+Mmap.draw_map3()
 while is_move != 'y':
-    is_move = input(Lmap.draw_wait_fowrd())
+    is_move = input(Mmap.draw_wait_fowrd())
     if is_move == 'y':
         break
 
 encount += 1
-is_win = LBbattle_main.run_battle(encount) #LBattle_mainを呼び出す
+is_win = Mbattle_main.run_battle(encount) #LBattle_mainを呼び出す
 if is_win == 'win':
-    Lresult.draw_wining()
+    Mresult.draw_wining()
 elif is_win == 'lose':
-    Lresult.draw_losing()
+    Mresult.draw_losing()
